9b1cfda8f5c66df217901ffd11d79308849f2dab
efp-bsc-com-01-24-dotcom <efp-bsc-com-01-24@unima.ac.mw>
2026-03-29 19:28:45 +0200

    modified: package-lock.json, package.json, src/app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'yourpassword',
+      database: 'testdb',
+      autoLoadEntities: true,
+      synchronize: true, // ⚠️ only for dev
+    }),
+  ],
 })
-export class AppModule {}
+export class AppModule{}
