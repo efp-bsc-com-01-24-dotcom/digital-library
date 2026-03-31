03c8937489239e526b73e9e2143631595a2f8935
efp-bsc-com-01-24-dootcom <efp-bsc-com-01-24@unima.ac.mw>
2026-03-31 15:15:04 +0200

    modified : some files

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { post } from 'axios';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,13 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+    
   }
+  @Get('/name')
+  getName(): string {
+    return this.appService.getName();
+    
+  }
+ 
+ 
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { get } from 'axios';
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'jesus loves you ♥';
+    return 'jesus loves you ♥ ';
   }
+
+  getName(): string {
+    return 'ephraim';
+  }
+   
 }
