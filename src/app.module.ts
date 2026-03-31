fa13989dc5613edd9ad6ec504a736215f471e118
efp-bsc-com-01-24-dootcom <efp-bsc-com-01-24@unima.ac.mw>
2026-03-31 13:53:09 +0200

    Revert "modified: package-lock.json, package.json, src/app.module.ts"
    This reverts commit 9b1cfda8f5c66df217901ffd11d79308849f2dab.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,10 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'yourpassword',
-      database: 'testdb',
-      autoLoadEntities: true,
-      synchronize: true, // ⚠️ only for dev
-    }),
-  ],
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
-export class AppModule{}
+export class AppModule {}
